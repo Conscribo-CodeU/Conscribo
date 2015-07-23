43cfa286c6143eb3da822cb66059d66afd2763da
DeveloperTommy <its.tommy.nguyen@gmail.com>
2015-07-22 23:39:44 -0400

    Very basic Google Sign-In added. No sign-out added yet or manipulation of data

--- app/src/main/java/com/codeu/app/conscribo/MainActivity.java
+++ app/src/main/java/com/codeu/app/conscribo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.codeu.app.conscribo;
 
+import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -25,6 +28,14 @@ public class MainActivity extends ActionBarActivity implements GoogleApiClient.C
     /* Client used to interact with Google APIs. */
     private GoogleApiClient mGoogleApiClient;
 
+    /* Is there a ConnectionResult resolution in progress? */
+    private boolean mIsResolving = false;
+
+    /* Should we automatically resolve ConnectionResults when possible? */
+    private boolean mShouldResolve = false;
+
+    private final String G_SIGN_IN_TAG = "Google Sign In: ";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,11 +57,25 @@ public class MainActivity extends ActionBarActivity implements GoogleApiClient.C
                 .addApi(Plus.API)
                 .addScope(new Scope(Scopes.PROFILE))
                 .build();
+
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        // ...
+        if (v.getId() == R.id.sign_in_button) {
+            onSignInClicked();
+        }
+    }
+
+    private void onSignInClicked() {
+        // User clicked the sign-in button, so begin the sign-in process and automatically
+        // attempt to resolve any errors that occur.
+        mShouldResolve = true;
+        mGoogleApiClient.connect();
+
+        // Show a message to the user that we are signing in.
+        //mStatusTextView.setText(R.string.signing_in);
     }
 
     @Override
@@ -60,12 +85,42 @@ public class MainActivity extends ActionBarActivity implements GoogleApiClient.C
 
     @Override
     public void onConnected(Bundle bundle) {
+        // onConnected indicates that an account was selected on the device, that the selected
+        // account has granted any requested permissions to our app and that we were able to
+        // establish a service connection to Google Play services.
+        Log.d(G_SIGN_IN_TAG, "onConnected:" + bundle);
+        mShouldResolve = false;
 
+        // Show the signed-in UI
+        //showSignedInUI();
     }
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
+        // Could not connect to Google Play Services.  The user needs to select an account,
+        // grant permissions or resolve an error in order to sign in. Refer to the javadoc for
+        // ConnectionResult to see possible error codes.
+        Log.d(G_SIGN_IN_TAG, "onConnectionFailed:" + connectionResult);
 
+        if (!mIsResolving && mShouldResolve) {
+            if (connectionResult.hasResolution()) {
+                try {
+                    connectionResult.startResolutionForResult(this, RC_SIGN_IN);
+                    mIsResolving = true;
+                } catch (IntentSender.SendIntentException e) {
+                    Log.e(G_SIGN_IN_TAG, "Could not resolve ConnectionResult.", e);
+                    mIsResolving = false;
+                    mGoogleApiClient.connect();
+                }
+            } else {
+                // Could not resolve the connection result, show the user an
+                // error dialog.
+                //showErrorDialog(connectionResult);
+            }
+        } else {
+            // Show the signed-out UI
+            //showSignedOutUI();
+        }
     }
 
     @Override
@@ -102,4 +157,21 @@ public class MainActivity extends ActionBarActivity implements GoogleApiClient.C
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(G_SIGN_IN_TAG, "onActivityResult:" + requestCode + ":" + resultCode + ":" + data);
+
+        if (requestCode == RC_SIGN_IN) {
+            // If the error resolution was not successful we should not resolve further.
+            if (resultCode != RESULT_OK) {
+                mShouldResolve = false;
+            }
+
+            mIsResolving = false;
+            mGoogleApiClient.connect();
+        }
+    }
+
 }
